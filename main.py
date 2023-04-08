3ff887d1ffd410a3c2324b33c04fe89e3f393d34
Wasif Bala <wbala@rad-res-lt16.local>
2023-04-07 23:46:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import streamlit as st
+import clipboard
+from run_model import run_model
+
+# set API key
+os.environ["OPENAI_API_KEY"] = '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'
+
+# Set up the basic layout
+st.set_page_config(page_title="Language Model Interface", layout="wide")
+
+# Add the navigation bar
+st.title("Radiology Auto-Template Generator")
+
+# Add a reset button
+if st.button("Reset"):
+    st.experimental_rerun()
+
+# Create two columns, split the screen into two columns
+left_column, middle_column = st.columns(2)
+
+# Set up the user input column
+with left_column:
+    st.header("Preliminary Report")
+    prelim_input = st.text_area("Enter your text here:")
+
+# Set up the template column
+with middle_column:
+    st.header("Requested Template")
+    template_input = st.text_area("Enter your template here:")
+
+# Add a submit button
+submit_button = st.button("Submit")
+
+# Display the model output header
+st.header("Model Output")
+
+if submit_button:
+    model_output = run_model(prelim_input=prelim_input, template_input=template_input)
+    st.write(model_output) 
+
+    # Add a button to copy the model output to the clipboard
+    if st.button("Copy to Clipboard"):
+        try:
+            # Copy the model output to the clipboard
+            clipboard.copy(model_output)
+            st.success("Model output copied to clipboard!")
+        except Exception as e:
+            st.error(f"An error occurred while copying to clipboard: {str(e)}")
---
+++ run_model.py
@@ -0,0 +1,42 @@
+import os
+
+# langchain
+import langchain
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain, PromptTemplate
+
+
+def run_model(prelim_input, template_input):
+
+    template = """
+
+    You are Assistant, a large language model trained by OpenAI that focuses on assiting radiologists with their work. \
+    In particular, Assistant is an expert at taking a preliminary report from a radiologist and converting it into a templated \
+    standard report format for easy readability by other clinicians. Onlly replace or remove any words unless it woould be \
+    contradictory to include them.
+
+
+    The only goal of the Assistant is to produce the templated report in the style of the provided template. This is the only response.
+
+    Preliminary Report: {prelim}
+
+    Template: {rads_template}
+
+    """
+
+    prompt = PromptTemplate(
+        input_variables = ["prelim", "rads_template"],
+        template=template)
+
+
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    prompt_inputs = {
+        'prelim': prelim_input,
+        'rads_template': template_input
+    }
+
+    output = chain.run(prompt_inputs)
+
+    return output

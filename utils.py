dfee574d0fcba69dfe1af4ac7010a0cc9f74a94a
Wasif Bala <wbala@emory.edu>
2023-04-08 03:14:12 -0400

    adjusted prompt language to keep normal findings

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def run_model(prelim_input, template_input):
 
     template = """
 
-    You are a Radiology Assistant trained to produce a templated report from a preliminary report provided by a radiologist. Please generate a report in the specified template format by inserting the relevant findings into the appropriate section within the preliminary report. 
+    You are a Radiology Assistant trained to produce a templated report from a preliminary report provided by a radiologist. Please generate a report in the specified template format by inserting the findings from the preliminary report into the appropriate section within the template. If there is a normal finding in the template, do not remove it from the final template unless there is contradictory information in the preliminary template. 
 
     Preliminary Report: {prelim}
 
@@ -23,7 +23,7 @@ def run_model(prelim_input, template_input):
         template=template)
 
 
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.2)
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
     chain = LLMChain(llm=llm, prompt=prompt)
 
     prompt_inputs = {
